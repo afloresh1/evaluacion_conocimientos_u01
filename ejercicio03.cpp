044b37dba1400aab5393bdbaa0c658d88becded4
afloresh1 <afloresh@unjbg.edu.pe>
2024-06-24 00:42:06 -0500

    Agregando tercer archivo de la evaluación de conocimientos

---
+++ ejercicio03.cpp
@@ -0,0 +1,31 @@
+//Alison Khiara Flores Hallasi//
+//Tercer ejercicio
+//Calcule la sumatoria de las fracciones
+#include <iostream>
+using namespace std;
+double suma(int a);
+float factorial (int b);
+int main(){
+	int n, sum=0;
+	cout<<"Ingrese el numero enesimo: "; cin>>n;
+	sum = suma(n);
+	cout<<"La suma especial es: "<<sum<<endl;	
+	return 0;
+}
+// Función para calcular la suma
+double suma(int a){
+	double s=0.0;
+	for (int i=1; i<=a; i++){
+	
+		s= s + factorial(i)/(2*i);
+	}
+	return (s);
+}
+// Función para calcular el factorial
+float factorial (int b){
+	float f=1;
+	for (int i=2; i<=b; i++){
+		f*=i;
+	}
+	return (f);
+}
