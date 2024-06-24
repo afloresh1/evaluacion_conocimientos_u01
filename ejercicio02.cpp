1ddd3af8b7e545846002a286ea65a499be160cc4
afloresh1 <afloresh@unjbg.edu.pe>
2024-06-23 22:51:31 -0500

    Se agregó una funcion para ordenar el código en donde se clasifica por el numero de veces que era verdadera la condición

--- ejercicio02.cpp
+++ ejercicio02.cpp
@@ -1,22 +1,38 @@
 //Alison Khiara Flores Hallasi//
 //Segundo ejercicio
 //Diseñe un algoritmo que determine si dos números son PESI
+
 #include <iostream>
 using namespace std;
+//funciones del while para regresar
+int funcion1(int n1, int n2);
+
+
 int main(){
-	int n1, n2, c=0, i=1;
-	cout<<"Ingrese los números a verificar: "; cin>>n1;
-	cin>>n2;
-	while((i<=n1)&&(i<=n2)){
-		if((n1%i==0)&&(n2%i==0)){
-			c = c+1;
-		}
-		i=i+1;
-	}
+	int n1, n2, c=0;
+	cout<<"Ingrese el primer numero a verificar: "; cin>>n1;
+	cout<<"Ingrese el segundo numero a verificar: "; cin>>n2;
+	
+	c = funcion1(n1, n2);
+	
 	if (c>1){
-		cout<<"No son PESI"<<endl;}
+		cout<<"No son PESI"<<endl;
+		}
 	else {
 		cout<<"Son PESI"<<endl;
 	}
 	
 }
+
+	int funcion1(int n1, int n2){
+		int i=1, c=0;
+		
+		while((i<=n1)&&(i<=n2)){
+			if((n1%i==0)&&(n2%i==0)){
+				c = c+1;
+			}
+			i=i+1;
+		}	
+		return (c);
+	}
+	
