4d03f2d862f8f53138b4cf545510e3ead6e8a3c3
afloresh1 <afloresh@unjbg.edu.pe>
2024-06-23 21:19:49 -0500

    Agregando primer ejercicio

---
+++ ejercicio01.cpp
@@ -0,0 +1,34 @@
+//Alison Khiara Flores Hallasi//
+//Primer ejercicio
+//Diseñe una calculadora
+#include <iostream>
+using namespace std;
+int main(){
+	int op1, op2; //dato de entrada
+	int r; //dato de salida
+	char op;//dato de entrada
+	cout<<"Ingrese los operandos: "; 
+	cin>>op1;
+	cin>>op2;
+	cout<<"Ingrese el operador: ";
+	cin>>op;
+	switch (op){
+		case '+':
+			r=op1+op2;
+			cout<<r;
+			break;
+		case '-':
+			r=op1-op2;
+			cout<<r;
+			break;
+		case '*':
+			r=op1*op2;
+			cout<<r;
+			break;
+		case '/':
+			r=op1/op2;
+			cout<<r;
+			break;
+	}
+	return 0;
+}
