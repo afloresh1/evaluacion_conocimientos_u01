6bdb3e86540b18b5bed0a1876e1fb72505a1e1ad
afloresh1 <afloresh@unjbg.edu.pe>
2024-06-24 00:48:05 -0500

    Se le cambio el tipo de variable que sería en la función factorial, abreviandolo asi con un operador static_cast

--- ejercicio03.cpp
+++ ejercicio03.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 double suma(int a);
-float factorial (int b);
+int factorial (int b);
 int main(){
 	int n, sum=0;
 	cout<<"Ingrese el numero enesimo: "; cin>>n;
@@ -17,13 +17,13 @@ double suma(int a){
 	double s=0.0;
 	for (int i=1; i<=a; i++){
 	
-		s= s + factorial(i)/(2*i);
+		s= s + static_cast<double>(i)/(2*i);
 	}
 	return (s);
 }
 // Función para calcular el factorial
-float factorial (int b){
-	float f=1;
+int factorial (int b){
+	int f=1;
 	for (int i=2; i<=b; i++){
 		f*=i;
 	}
